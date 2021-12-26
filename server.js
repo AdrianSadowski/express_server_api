3fc0118a04ac2d2eebb2c0174c3ea07d1dbf332e
AdrianSadowski <sadow.adrian@gmail.com>
2021-12-26 15:58:14 +0100

    Add seats and concerts routing

--- server.js
+++ server.js
@@ -1,66 +1,25 @@
-const { text } = require('express');
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const { v4: uuidv4 } = require('uuid');
+
+
+const testimonialsRoutes = require('./routes/testimonials.routes');
+const concertsRoutes = require('./routes/concerts.routes');
+const seatsRoutes = require('./routes/seats.routes');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// baza danych
-const db = [
-  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-  { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-];
-
-// get function
-
-app.get('/testimonials', (req, res) => {
-  res.json(db);
-});
-
-app.get('/testimonials/:id', (req, res) => {
-  res.json(db.filter((item) => item.id == req.params.id));
-});
-
-app.get('/testimonials/random', (req, res) => {
-  let item = db[Math.floor(Math.random() * db.length )];
-  res.json(item);
-});
-
-app.post('/testimonials', (req, res) => {
-  const newData = {
-    id: uuidv4(),
-    author: req.body.author,
-    text: req.body.text,
-  };
-  db.push(newData);
-  return res.json({message: 'ok'});
-});
-
-app.put('/testimonials/:id', (req, res) => {
-  const editedTestimonials = db.filter((item) => item.id == req.params.id);
-  const indexOfTestimonial = db.indexOf(editedTestimonials);
-  const newTestimonials = {
-    ...editedTestimonials,
-    author: req.body.author,
-    text: req.body.text,
-  };
-  db[indexOfTestimonial] = newTestimonials;
-  return res.json({message: 'ok'});
-});
-
-app.delete('/testimonials/:id', (req, res) => {
-  const editedTestimonials = db.filter((item) => item.id == req.params.id);
-  const indexOfTestimonial = db.indexOf(editedTestimonials);
-  db.splice(indexOfTestimonial, 1);
-  return res.json({message: 'ok'});
-});
-
-
-app.use((req, res) => {
-  res.status(404).send('404 not found...');
-})
+app.use(cors());
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
+});
+
+
+app.use('/api', testimonialsRoutes);
+app.use('/api', concertsRoutes);
+app.use('/api', seatsRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({message: '404 not found...'});
 });
