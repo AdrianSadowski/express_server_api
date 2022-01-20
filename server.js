f47ff1470a978db22301c660bd4e495a8b0395e6
AdrianSadowski <sadow.adrian@gmail.com>
2022-01-20 19:50:59 +0100

    Connect testimonidal with mongoose DB

--- server.js
+++ server.js
@@ -2,24 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const path = require('path');
-const socket = require('socket.io');
+const mongoose = require('mongoose');
+
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  req.io = io;
-  next();
-});
+app.use;
+
 
 app.use('/api', testimonialsRoutes);
-app.use('/api', concertsRoutes);
-app.use('/api', seatsRoutes);
+// app.use('/api', concertsRoutes);
+// app.use('/api', seatsRoutes);
 app.use(express.static(path.join(__dirname, '/client/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
@@ -31,16 +30,19 @@ app.use((req, res) => {
 });
 
 // server
-const server = app.listen(process.env.PORT || 8000, () => {
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', (err) => console.log('Error' + err));
+
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running');
 });
 
-const io = socket(server);
 
-io.on('connection', (socket) => {
-  console.log('a user connected ' + socket.id);
-
-  socket.on('disconnect', () => {
-    console.log('Socket ' + socket.id + ' left');
-  });
-});
