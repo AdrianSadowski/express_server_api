7ec145f7dba7ba6547b1d9c8ed874dc093a4592f
AdrianSadowski <sadow.adrian@gmail.com>
2022-01-22 15:32:58 +0100

    With socket problem

--- server.js
+++ server.js
@@ -9,11 +9,14 @@ const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use;
 
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
